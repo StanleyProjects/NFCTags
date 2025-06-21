d49b6778fab7123baf70751279e1200b086fbc3d
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-06-21 14:57:33 +0300

    init sample

---
+++ sample/src/main/kotlin/sp/sample/nfctags/App.kt
@@ -0,0 +1,10 @@
+package sp.sample.nfctags
+
+import android.app.Application
+
+internal class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // todo
+    }
+}
---
+++ sample/src/main/kotlin/sp/sample/nfctags/MainActivity.kt
@@ -0,0 +1,12 @@
+package sp.sample.nfctags
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import sp.ax.nfctags.NFCTags
+
+internal class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val tags: NFCTags? = null // todo
+    }
+}
