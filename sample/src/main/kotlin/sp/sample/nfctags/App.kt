701f7dc0728292435ef37e9ef060e5087284eb23
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-06-21 15:12:47 +0300

    init compose

--- sample/src/main/kotlin/sp/sample/nfctags/App.kt
+++ sample/src/main/kotlin/sp/sample/nfctags/App.kt
@@ -1,10 +1,16 @@
 package sp.sample.nfctags
 
 import android.app.Application
+import sp.ax.nfctags.NFCTags
 
 internal class App : Application() {
     override fun onCreate() {
         super.onCreate()
         // todo
+    }
+
+    companion object {
+        private var _tags: NFCTags? = null
+        val tags: NFCTags get() = checkNotNull(_tags) { "No tags!" }
     }
 }
--- sample/src/main/kotlin/sp/sample/nfctags/MainActivity.kt
+++ sample/src/main/kotlin/sp/sample/nfctags/MainActivity.kt
@@ -2,11 +2,15 @@ package sp.sample.nfctags
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import sp.ax.nfctags.NFCTags
+import androidx.compose.ui.platform.ComposeView
 
 internal class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val tags: NFCTags? = null // todo
+        val view = ComposeView(this)
+        setContentView(view)
+        view.setContent {
+            MainScreen()
+        }
     }
 }
