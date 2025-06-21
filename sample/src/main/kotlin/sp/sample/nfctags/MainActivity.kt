1a2cc70a7137e8782a8216244d735dea7a4539e9
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-06-21 22:00:34 +0300

    compose

--- sample/src/main/kotlin/sp/sample/nfctags/MainActivity.kt
+++ sample/src/main/kotlin/sp/sample/nfctags/MainActivity.kt
@@ -1,90 +1,16 @@
 package sp.sample.nfctags
 
-import android.nfc.tech.IsoDep
 import android.os.Bundle
-import android.view.Gravity
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.FrameLayout
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.lifecycle.coroutineScope
-import kotlinx.coroutines.launch
-import sp.ax.nfctags.NFCTags
-import sp.kx.bytes.toHEX
+import androidx.compose.ui.platform.ComposeView
 
 internal class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val view = ComposeView(this)
-//        setContentView(view)
-//        view.setContent {
-//            MainScreen()
-//        }
-        val root = LinearLayout(this).also {
-            it.layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT,
-            )
-            it.orientation = LinearLayout.VERTICAL
-            it.gravity = Gravity.CENTER_VERTICAL
-        }
-        val button = Button(this).also {
-            it.layoutParams = FrameLayout.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-            )
-            root.addView(it)
-        }
-        val tv = TextView(this).also {
-            it.layoutParams = FrameLayout.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-            )
-            root.addView(it)
-        }
-        setContentView(root)
-        val tags = App.tags
-        val activity: ComponentActivity = this
-        lifecycle.coroutineScope.launch {
-            tags.states.collect { state ->
-                println("[MainActivity]:nfc:state: $state") // todo
-                when (state) {
-                    NFCTags.State.Connected -> {
-                        tv.visibility = View.VISIBLE
-                    }
-                    else -> {
-                        tv.visibility = View.INVISIBLE
-                    }
-                }
-                when (state) {
-                    NFCTags.State.Searching, NFCTags.State.Waiting, NFCTags.State.Connected -> {
-                        button.text = "stop"
-                        button.setOnClickListener { _ ->
-                            tags.stop()
-                        }
-                    }
-                    NFCTags.State.Stopped -> {
-                        button.text = "start"
-                        button.setOnClickListener { _ ->
-                            tags.start(activity = activity)
-                        }
-                    }
-                }
-            }
-        }
-        lifecycle.coroutineScope.launch {
-            tags.events.collect { event ->
-                when (event) {
-                    is NFCTags.Event.OnTag -> {
-                        println("[MainActivity]:nfc:event:tag: ${event.tag.id.toHEX()}") // todo
-                        tags.connect(tag = event.tag)
-                        tv.text = event.tag.id.toHEX()
-                    }
-                }
-            }
+        val view = ComposeView(this)
+        setContentView(view)
+        view.setContent {
+            MainScreen()
         }
     }
 }
--- sample/src/main/kotlin/sp/sample/nfctags/MainScreen.kt
+++ sample/src/main/kotlin/sp/sample/nfctags/MainScreen.kt
@@ -1,5 +1,8 @@
 package sp.sample.nfctags
 
+import android.nfc.Tag
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.LocalActivity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -10,13 +13,40 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.repeatOnLifecycle
+import sp.ax.nfctags.NFCTags
+import sp.kx.bytes.toHEX
 
 @Composable
 internal fun MainScreen() {
+    val lifecycle = LocalLifecycleOwner.current.lifecycle
+    val tags = App.tags
+    val state: NFCTags.State = tags.states.collectAsStateWithLifecycle(minActiveState = Lifecycle.State.RESUMED).value
+    val tagState = remember { mutableStateOf<String?>(null) }
+    LaunchedEffect(Unit) {
+        lifecycle.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+            tags.events.collect { event ->
+                when (event) {
+                    is NFCTags.Event.OnTag -> {
+                        println("[MainScreen]:nfc:event:tag: ${event.tag.id.toHEX()}") // todo
+                        tagState.value = event.tag.id.toHEX()
+                        tags.connect(tag = event.tag)
+                    }
+                }
+            }
+        }
+    }
+    val activity = LocalActivity.current as? ComponentActivity ?: TODO()
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -31,11 +61,31 @@ internal fun MainScreen() {
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(48.dp)
+                    .wrapContentSize(),
+                text = state.name,
+            )
+            BasicText(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
                     .clickable {
-                        // todo
+                        when (state) {
+                            NFCTags.State.Stopped -> tags.start(activity = activity)
+                            else -> tags.stop()
+                        }
                     }
                     .wrapContentSize(),
-                text = "test",
+                text = when (state) {
+                    NFCTags.State.Stopped -> "start"
+                    else -> "stop"
+                },
+            )
+            BasicText(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+                    .wrapContentSize(),
+                text = if (state == NFCTags.State.Connected) tagState.value.orEmpty() else "",
             )
         }
     }
